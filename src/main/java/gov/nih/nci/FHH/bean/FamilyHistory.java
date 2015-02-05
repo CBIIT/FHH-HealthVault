1cd11e9cad2bb0263a5ede392c93aed8604cb85f
jonnalah <jonnalah@NCI-01870531-L.nci.nih.gov>
2015-02-05 10:29:50 -0500

    Fixed a bug where save to health vault creates copy of disease for a new disease added for cause of death.

--- src/main/java/gov/nih/nci/FHH/bean/FamilyHistory.java
+++ src/main/java/gov/nih/nci/FHH/bean/FamilyHistory.java
@@ -901,14 +901,14 @@ public class FamilyHistory implements Serializable, thing{
 			        			//Remove the condition from the conditions list if the condition has a code
 			        			String conditionCode = conditionMap.get("conditionCode").toString();
 			        			String conditionName = conditionMap.get("condition").toString();
-			        			if(conditionCode.matches("^[0-9]*$")){
+//			        			if(conditionCode.matches("^[0-9]*$")){
 			        				Iterator<Map<String, Object>> itr = conditions.iterator();
 			    					while(itr.hasNext()){
 			    						Map<String, Object> condMap = itr.next();
 			        					if(conditionName.equals(condMap.get("condition"))){
 			        						itr.remove();
 			        					}
-			        				}
+//			        				}
 //			        				conditions.remove(conditions.remove(conditions.size()-1));
 			        			}
 			        			Node sourceOf = condition.getNextSibling();
